b1e69d9c4ace76c1bae9f7863f3dee2fbdf997f8
jizapika <lyovoch@bk.ru>
2023-08-05 22:11:21 +0300

    feat: add lab-1

---
+++ public/Web-Programming-Course/README.md
@@ -0,0 +1,9 @@
+# Web-Programming-Course
+
+Чечулин Лев Олегович, М33021, 311460.
+
+Макет сайта.
+
+![avatar](src/template.bmp)
+bla-bla-bla
+blo-blo-blo
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public/Web-Programming-Course'));
+
   const defaultPort = 3000;
   const radix = 10;
   await app.listen(
