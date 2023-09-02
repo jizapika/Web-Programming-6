d1c9419fb3420662ddbd09ed0345b982b2438c80
jizapika <lyovoch@bk.ru>
2023-09-02 14:19:49 +0300

    feat: add lab-3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { join } from "path";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: ".env"
+  }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (cfg: ConfigService) => ({
+        type: "postgres",
+        host: cfg.get("DB_HOST"),
+        port: cfg.get("DB_PORT"),
+        username: cfg.get("DB_USER"),
+        password: cfg.get("DB_PASS"),
+        database: cfg.get("DB_NAME"),
+        extra: {
+          ssl: ["false"]
+        },
+        entities: [join(__dirname, "**", "*.entity.{ts,js}")],
+        synchronize: true
+      })
+    })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
